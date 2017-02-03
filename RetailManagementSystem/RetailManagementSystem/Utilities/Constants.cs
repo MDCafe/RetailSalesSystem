d9afdc6fdddca5b258b38bd08873496ee9473193
mdcafe <goku.sumi@gmail.com>
2017-02-03 21:57:11 +0530

    added constants

---
+++ RetailManagementSystem/RetailManagementSystem/Utilities/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManagementSystem.Utilities
+{
+    static class Constants
+    {
+        public const string AMOUNT = "Amount";
+        public const string CUSTOMERS_OTHERS = "Other Customers";
+        public const string CUSTOMERS_HOTEL = "Hotels";
+        //public const string DISCOUNT_PERCENT = "DiscountPercentage";
+        //public const string DISCOUNT_AMT = "DiscountAmount";
+
+    }
+}
