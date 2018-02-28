b4dc792413e5b514fc38de2617e6fdac802dfb56
Goku <goku.sumi@gmail.com>
2018-02-28 09:24:57 +0530

    e

--- Database/DuplicateRows.sql
+++
@@ -1,8 +0,0 @@
-use RMS;(1469,1475,1618)
-
-SELECT * FROM PriceDetails	WHERE ProductId in
-(
-SELECT ProductId
-FROM PriceDetails
-GROUP BY ProductId, Price
-HAVING count(*) > 1)
--- Database/StockQuery.sql
+++
@@ -1,9 +0,0 @@
-select p.Id as productId,p.Code oldcode,p.Name,ReorderPoint,c.id categoryId,c.name,pd.PriceId, pd.Price,pd.SellingPrice,p.CompanyId,cp.Name,
-COALESCE(st.ClosingBalance,s.Quantity) Quantity
-	from products p Join category c on c.Id = p.CategoryId
-					Join Companies cp on cp.Id = p.CompanyId
-					Join PriceDetails pd on pd.ProductId = p.Id
-                    Join Stocks s on s.ProductId = p.Id and s.PriceId = pd.PriceId
-					left Join StockTransaction st on st.StockId = s.Id and date(st.addedOn) = '2017-11-21'		
-    
-   
