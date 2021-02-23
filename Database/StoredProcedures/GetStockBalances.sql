e69c09b48e5ac2517d6e3a14f9831fa557f0fcce
Gokumarasu Moorthy <goku.sumi@gmail.com>
2021-02-23 15:53:25 +0530

    Added Stock value to the report

--- Database/StoredProcedures/GetStockBalances.sql
+++ Database/StoredProcedures/GetStockBalances.sql
@@ -9,37 +9,44 @@ BEGIN
 if(categoryId = 0 and productId = 0 and companyId = 0)
 then
     select p.Id as productId,p.Name,ReorderPoint,c.name CategoryName,cp.Name CompanyName,
-	sum(s.quantity) Quantity
+	sum(s.quantity) Quantity,pr.Price CostPrice, Quantity * pr.Price Value
 	from products p Join category c on c.Id = p.CategoryId
                     Join Stocks s on s.ProductId = p.Id
+                    join pricedetails pr on pr.PriceId = s.PriceId
                     Join Companies cp on cp.id = p.CompanyId
+	where p.IsActive = true
 	group by p.id;
 					
 elseif (categoryId !=0 and productId = 0 and companyId = 0)
 then
  select p.Id as productId,p.Name,ReorderPoint,c.name CategoryName,cp.Name CompanyName,
-	sum(s.quantity) Quantity
+	sum(s.quantity) Quantity,pr.Price CostPrice, Quantity * pr.Price Value
 	from products p Join category c on c.Id = p.CategoryId and c.Id = categoryId
                     Join Stocks s on s.ProductId = p.Id
+                    join pricedetails pr on pr.PriceId = s.PriceId
                     Join Companies cp on cp.id = p.CompanyId
-	group by p.id;		
+	where p.IsActive = true
+    group by p.id;		
     
 elseif (categoryId =0 and productId != 0 and companyId = 0)
 then
 	select p.Id as productId,p.Name,ReorderPoint,c.name CategoryName,cp.Name CompanyName,
-	sum(s.quantity) Quantity
+	sum(s.quantity) Quantity,pr.Price CostPrice, Quantity * pr.Price Value
 	from products p Join category c on c.Id = p.CategoryId 
                     Join Stocks s on s.ProductId = p.Id
+                    join pricedetails pr on pr.PriceId = s.PriceId
                     Join Companies cp on cp.id = p.CompanyId
-    where p.Id = productId                
+    where p.Id = productId and p.IsActive = true               
 	group by p.id;	
 elseif (categoryId =0 and productId = 0 and companyId != 0)
 then
 	select p.Id as productId,p.Name,ReorderPoint,c.name CategoryName,cp.Name CompanyName,
-	sum(s.quantity) Quantity
+	sum(s.quantity) Quantity,pr.Price CostPrice, Quantity * pr.Price Value
 	from products p Join category c on c.Id = p.CategoryId 
                     Join Stocks s on s.ProductId = p.Id
+                    join pricedetails pr on pr.PriceId = s.PriceId
                     Join Companies cp on cp.id = p.CompanyId and cp.Id = companyId            
+    where p.IsActive = true
 	group by p.id;	
 end if;    
 
